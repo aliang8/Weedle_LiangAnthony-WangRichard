df4e832b15834e1796000606b3b04bd25244c16d
Anthony Liang <aliang8@stuy.edu>
2016-10-18 16:10:28 -0400

    finished

--- get_averages.py
+++ get_averages.py
@@ -11,13 +11,23 @@ c = db.cursor()    #facilitate db ops
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 #...perhaps by beginning with these examples...
 
+d = {}
 q = "SELECT name, students.id, mark FROM courses, students WHERE students.id = courses.id;"
-c.execute(q)   
-results = c.fetchall()
-d = {};
+results = c.execute(q)   
 for row in results:
-    
-    
+    name = row[0]
+    d[name] = [0,0,0]
+results = c.execute(q)
+for rows in results:
+    name = rows[0]
+    id = rows[1]
+    grade = rows[2]
+    d[name][0] += grade
+    d[name][1] = id
+    d[name][2] += 1
+for key in d:
+    print "Name: "+ key + " ID: " + str(d[key][1]) + " Average: " + str(float(d[key][0])/d[key][2]) 
+
 
 #==========================================================
 db.commit() #save changes
